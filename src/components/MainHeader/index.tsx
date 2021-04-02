d32aa97b75897ea32bfd68a08f024934be3525e2
LuizTimboAlcantara <joseluiztimbo@gmail.com>
2021-04-02 17:24:38 -0300

    Adicionando toggle no MainHeader.

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo } from "react";
 
+import Toggle from "../Toggle";
 import emojis from "../../utils/emojis";
 
 import { Container, Profile, Welcome, UserName } from "./styles";
@@ -12,7 +13,7 @@ const MainHeader: React.FC = () => {
 
   return (
     <Container>
-      <h1>Toogle</h1>
+      <Toggle />
 
       <Profile>
         <Welcome>Olá, {emoji}</Welcome>
