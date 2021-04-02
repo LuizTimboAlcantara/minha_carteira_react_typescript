de87b09cd9d2952d35763d4347ddeab91929fab7
LuizTimboAlcantara <joseluiztimbo@gmail.com>
2021-04-02 20:14:11 -0300

    Layout dos filtros recorrentes e eventuais.

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -4,20 +4,36 @@ import ContentHeader from "../../components/ContentHeader";
 import SelectInput from "../../components/SelectInput";
 import HistoryFinanceCard from "../../components/HistoryFinanceCard";
 
-import { Container, Content } from "./styles";
+import { Container, Content, Filters } from "./styles";
 
 const List: React.FC = () => {
-  const options = [
-    { value: "Luiz", label: "Luiz" },
-    { value: "Timbó", label: "Timbó" },
-    { value: "Alcântara", label: "Alcântara" },
+  const months = [
+    { value: 7, label: "Julho" },
+    { value: 8, label: "Agosto" },
+    { value: 9, label: "Setembro" },
+  ];
+
+  const years = [
+    { value: 2020, label: 2020 },
+    { value: 2019, label: 2019 },
+    { value: 2018, label: 2018 },
   ];
 
   return (
     <Container>
       <ContentHeader title="Saídas" lineColor="#E44C4E">
-        <SelectInput options={options} />
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
+
+      <Filters>
+        <button type="button" className="tag-filter tag-filter-recurrent">
+          Recorrentes
+        </button>
+        <button type="button" className="tag-filter tag-filter-eventual">
+          Eventuais
+        </button>
+      </Filters>
 
       <Content>
         <HistoryFinanceCard tagColor="#E44C4E" title="Conta de Luz" subtitle="27/07/2020" amount="R$ 130" />
