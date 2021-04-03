b0976aa423a4b3fc3048e8eda468128ad21af3d0
LuizTimboAlcantara <joseluiztimbo@gmail.com>
2021-04-03 09:51:14 -0300

    Filtros de recorrentes e eventuais.

--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -17,6 +17,7 @@ export const Filters = styled.div`
       color: ${props => props.theme.colors.white};
       margin: 0 10px;
       transition: opacity 0.3s;
+      opacity: 0.4;
 
         &:hover{
           opacity: 0.7;
@@ -29,7 +30,7 @@ export const Filters = styled.div`
           display:block;
           width: 55px;
           margin: 0 auto;
-          border-bottom: 10px solid ${props => props.theme.colors.warning};
+          border-bottom: 10px solid ${props => props.theme.colors.success};
         }
     }
 
@@ -39,7 +40,11 @@ export const Filters = styled.div`
           display:block;
           width: 55px;
           margin: 0 auto;
-          border-bottom: 10px solid ${props => props.theme.colors.success};
+          border-bottom: 10px solid ${props => props.theme.colors.warning};
         }
     }
+
+        .tag-actived{
+          opacity: 1;
+        }
 `;
