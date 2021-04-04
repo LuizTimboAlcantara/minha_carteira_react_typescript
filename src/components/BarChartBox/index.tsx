b0518d96271fd981a18fdd6547d014312bb4a903
LuizTimboAlcantara <joseluiztimbo@gmail.com>
2021-04-03 22:19:22 -0300

    Finalizando gráficos.

--- src/components/BarChartBox/index.tsx
+++ src/components/BarChartBox/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ResponsiveContainer, BarChart, Bar, Cell, Tooltip } from "recharts";
 
-// import formatCurrency from "../../utils/formatCurrency";
+import formatCurrency from "../../utils/formatCurrency";
 
 import { Container, SideLeft, SideRight, LegendContainer, Legend } from "./styles";
 
@@ -34,12 +34,12 @@ const BarChartBox: React.FC<IBarChartBoxProps> = ({ title, data }) => {
       <SideRight>
         <ResponsiveContainer>
           <BarChart data={data}>
-            <Bar dataKey="amount">
+            <Bar dataKey="amount" name="Valor">
               {data.map((indicator) => (
                 <Cell key={indicator.name} fill={indicator.color} />
               ))}
             </Bar>
-            {/* <Tooltip formatter={(value: any) => formatCurrency(Number(value))} /> */}
+            <Tooltip cursor={{ fill: "none" }} formatter={(value: any) => formatCurrency(Number(value))} />
           </BarChart>
         </ResponsiveContainer>
       </SideRight>
