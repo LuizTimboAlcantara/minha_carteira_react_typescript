41f89a555f7968a9ada1215a02fd256016a9cff5
LuizTimboAlcantara <joseluiztimbo@gmail.com>
2021-04-04 15:49:49 -0300

    Deixando o Layout da aplicação responsivo.

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,18 +1,36 @@
-import React from "react";
-import { MdDashboard, MdArrowDownward, MdArrowUpward, MdExitToApp } from "react-icons/md";
+import React, { useState } from "react";
+import Toggle from "../Toggle";
+
+import { MdDashboard, MdArrowDownward, MdArrowUpward, MdExitToApp, MdClose, MdMenu } from "react-icons/md";
 
 import logoImg from "../../assets/logo.svg";
 
 import { useAuth } from "../../hooks/auth";
+import { useTheme } from "../../hooks/theme";
 
-import { Container, Header, LogImg, Title, MenuContainer, MenuItemLink, MenuItemButton } from "./styles";
+import { Container, Header, LogImg, Title, MenuContainer, MenuItemLink, MenuItemButton, ToggleMenu, ThemeToggleFooter } from "./styles";
 
 const Aside: React.FC = () => {
   const { signOut } = useAuth();
+  const { toggleTheme, theme } = useTheme();
+
+  const [toggleMenuIsOpened, setToggleMenuIsOpened] = useState(false);
+  const [darkTheme, setDarkTheme] = useState(() => (theme.title === "dark" ? true : false));
+
+  const handleToggleMenu = () => {
+    setToggleMenuIsOpened(!toggleMenuIsOpened);
+  };
+
+  const handleChangeTheme = () => {
+    setDarkTheme(!darkTheme);
+    toggleTheme();
+  };
 
   return (
-    <Container>
+    <Container menuIsOpen={toggleMenuIsOpened}>
       <Header>
+        <ToggleMenu onClick={handleToggleMenu}>{toggleMenuIsOpened ? <MdClose /> : <MdMenu />}</ToggleMenu>
+
         <LogImg src={logoImg} alt="Logo Minha Carteira" />
         <Title>Minha Carteira</Title>
       </Header>
@@ -22,19 +40,26 @@ const Aside: React.FC = () => {
           <MdDashboard />
           Dashboard
         </MenuItemLink>
+
         <MenuItemLink href="/list/entry-balance">
           <MdArrowUpward />
           Entradas
         </MenuItemLink>
+
         <MenuItemLink href="/list/exit-balance">
           <MdArrowDownward />
           Saídas
         </MenuItemLink>
+
         <MenuItemButton onClick={signOut}>
           <MdExitToApp />
           Sair
         </MenuItemButton>
       </MenuContainer>
+
+      <ThemeToggleFooter menuIsOpen={toggleMenuIsOpened}>
+        <Toggle labelLeft="Light" labelRight="Dark" checked={darkTheme} onChange={handleChangeTheme} />
+      </ThemeToggleFooter>
     </Container>
   );
 };
--- src/components/WalletBox/index.tsx
+++ src/components/WalletBox/index.tsx
@@ -33,7 +33,8 @@ const WalletBox: React.FC<IWalletBoxProps> = ({ title, amount, footerlabel, icon
     <Container color={color}>
       <span>{title}</span>
       <h1>
-        <CountUp end={amount} prefix={"R$ "} separator="." decimal="," decimals={2} />{" "}
+        <strong>R$ </strong>
+        <CountUp end={amount} separator="." decimal="," decimals={2} />{" "}
       </h1>
       <small>{footerlabel}</small>
       <img src={iconSelected} alt={title} />
